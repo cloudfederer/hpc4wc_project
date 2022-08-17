20f09a917ce166f59deedd058ae30a9d38882960
yiluchen1066 <15835221066@163.com>
2022-08-17 21:33:57 +0200

    running and scaling

--- shell_commands.sh
+++ shell_commands.sh
@@ -186,88 +186,93 @@ srun -n 8 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --p
 
 #strong scaling sized 128*128
 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 1 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 2 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 8 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
 
 #strong scaling sized 256*256
 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 1 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 2 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 4 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 8 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
 
 
 #strong scaling sized 512*512
 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 1 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 2 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 4 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 8 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
 
 # weak scaling 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 1 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=gt:gpu
+srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=gt:gpu
 
 #backend = gt:cuda
 #strong scaling sized 64 * 64
-srun -n 1 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --backend=cuda
+srun -n 1 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --
+backend=cuda
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --backend=cuda
+srun -n 2 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --
+backend=cuda
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --backend=cuda
+srun -n 4 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --
+backend=cuda
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --backend=cuda
+srun -n 8 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --
+backend=cuda
 
 #strong scaling sized 128*128
 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 1 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 2 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 8 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
 
 #strong scaling sized 256*256
 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 1 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 2 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 4 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 8 python stencil2d-gt4py-a4.py --nx 256 --ny 256 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
 
 
 #strong scaling sized 512*512
 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 1 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 2 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 2 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 4 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
-srun -n 8 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 8 python stencil2d-gt4py-a4.py --nx 512 --ny 512 --nz 64 --num_iter 64 --plot_result True --backend=cuda
 
 
 # weak scaling 
-srun -n 1 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 1 python stencil2d-gt4py-a4.py --nx 64 --ny 64 --nz 64 --num_iter 64 --plot_result True --
+backend=cuda
 
-srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True--backend=cuda
+srun -n 4 python stencil2d-gt4py-a4.py --nx 128 --ny 128 --nz 64 --num_iter 64 --plot_result True --backend=cuda
